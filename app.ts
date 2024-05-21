cc7bdf6d066b00576e80911f476eca6cd859ede5
Saeed Noor <163897168+saeednoor786@users.noreply.github.com>
2024-05-22 00:49:53 +0500

    push my assignment to git hub.

---
+++ app.ts
@@ -0,0 +1,8 @@
+// Q:2 Personal Message: Store a person’s name in a variable, and print a message to that person. 
+// Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+
+// Store the person's name in a variable
+let personName:string = "saeed  Noor";
+
+// / Print a personalized message to that person
+console.log(`Hello ${personName}, would you like to learn some python today?`);
